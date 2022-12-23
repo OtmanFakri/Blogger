543deffba7ed7e200bf9c15f1ed1fe423e3ebb6a
x2p <otmanhero00@gmail.com>
2022-12-23 11:14:33 -0500

    Oracle connect + auth

---
+++ account/auth.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+
+class OracleAuthBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return None
+        
+        if user.check_password(password):
+            return user
